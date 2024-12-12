5caad7a76ffa3511ae5ce4f33b781b5400cdf94e
xumh <960919993@qq.com>
2024-12-12 15:59:51 +0800

    测试gitigore规则

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Welcome to My Watchlist!'
+
+@app.route('/test')
+def hello1():
+    return 'Test!'
+
+@app.route('/test1')
+def hello2():
+    return 'Test1!'
